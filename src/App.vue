f4d72cf38e2048ac114fdf7fca166a2beb23b951
alpelenio <alpelenio@users.noreply.github.com>
2026-06-25 12:22:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ExampleTest from './components/ExampleTest.vue';
+</script>
+
+<template>
+  <ExampleTest />
+</template>
---
+++ src/components/ExampleTest.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello Lord</h1>
+    <p>tabangi tawn mi</p>
+  </div>
+</template>
